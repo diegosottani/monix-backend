8d667ba0738cae4ad25725c7b3f17317c0869a13
Diego Sottani <diegosottani@gmail.com>
2024-06-13 10:28:18 -0300

    Adicionando campo type_category no POST de categorias

--- src/api/auth/categories/post_user_category.js
+++ src/api/auth/categories/post_user_category.js
@@ -1,21 +1,26 @@
-import { supabase } from '../../../init';
+import { supabase } from "../../../init";
 
 export const post_user_category = async (req, res) => {
-  try {
+  const validTypeOfCategories = ["incoming", "expense", "investment"];
 
+  try {
     if (!req.body.name) {
-      res.status(400).json({ error: 'O nome da categoria é obrigatório' });
+      return res.status(400).json({ error: "O nome da categoria é obrigatório" });
+    }
+
+    if (!validTypeOfCategories.includes(req.body.type_category)) {
+      return res.status(400).json({ error: "Tipo de categoria inválido" });
     }
 
     const { error } = await supabase
-    .from('categories')
-    .insert({ user_id: req.user.id, name: req.body.name })
+      .from("categories")
+      .insert({ user_id: req.user.id, name: req.body.name, type_category: req.body.type_category });
 
     if (error) throw error;
 
-    res.status(200).send("Categoria criada com sucesso")
+    res.status(200).send("Categoria criada com sucesso");
   } catch (error) {
-    console.error('Erro ao criar categoria:', error);
-    res.status(500).json({ error: 'Erro ao recuperar categoria' });
+    console.error("Erro ao criar categoria:", error);
+    res.status(500).json({ error: "Erro ao criar categoria" });
   }
 };
