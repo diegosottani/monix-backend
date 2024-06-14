7ac61fa0db9a5d0858a6d5febad2f5677a9385b9
Diego Sottani <diegosottani@gmail.com>
2024-06-14 17:49:17 -0300

    Throw error se get categorias falhar

--- src/api/auth/categories/get_user_categories.js
+++ src/api/auth/categories/get_user_categories.js
@@ -9,6 +9,8 @@ export const get_user_categories = async (req, res) => {
     .eq('user_id', req.user.id)
     .eq("type_category", req.params.type);
 
+    if (error) throw error;
+
     res.status(200).send(data)
   } catch (error) {
     console.error('Erro ao recuperar categorias:', error);
