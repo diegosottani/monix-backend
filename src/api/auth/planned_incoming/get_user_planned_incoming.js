94a601c6555cb483388985f8cb066cf40ff27e41
Diego Sottani <diegosottani@gmail.com>
2024-07-31 11:26:17 -0300

    Alterações na rota get_user_planned_incoming

--- src/api/auth/planned_incoming/get_user_planned_incoming.js
+++ src/api/auth/planned_incoming/get_user_planned_incoming.js
@@ -2,24 +2,38 @@ import { supabase } from '../../../init';
 
 export const get_user_planned_incoming = async (req, res) => {
     try {
-        const { data, error } = await supabase
+        const month = req.query.month;
+        const year = req.query.year;
+
+        if (month === undefined || year === undefined) {
+            res.status(400).json({ error: 'É necessário preencher todos os campos obrigatórios' });
+            return;
+        }
+        
+        let query = supabase
             .from('planned_incoming')
             .select(`
                 id,
-                user_id (
-                    id,
-                    name
-                ),
+                user_id,
                 month,
                 year
             `)
             .eq('user_id', req.user.id);
+            
+        if(month != 0 && year != 0) {
+            query = query.eq('month', month);
+            query = query.eq('year', year);
+        } else {
+            query = query.eq('year', new Date().getFullYear());
+        }
+
+        const { data, error } = await query;
 
         if (error) throw error;
 
         res.status(200).json(data);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Erro ao recuperar entradas planejadas' });
+        res.status(500).json(error);
     }
 }
