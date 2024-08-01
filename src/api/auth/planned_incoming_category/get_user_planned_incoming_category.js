2e0907cd2c8502935a3e76ff5f120fca872ee859
Diego Sottani <diegosottani@gmail.com>
2024-08-01 11:11:09 -0300

    Correção na rota get_user_planned_incoming_category - filtrar por planned_id ao invés de user_id

--- src/api/auth/planned_incoming_category/get_user_planned_incoming_category.js
+++ src/api/auth/planned_incoming_category/get_user_planned_incoming_category.js
@@ -2,13 +2,13 @@ import { supabase } from "../../../init";
 
 export const get_user_planned_incoming_category = async (req, res) => {
   try {
-    const user_id = req.user.id;
+    const planned_id = req.query.planned_id;
     const category_id = req.query.category;
 
     const { data, error } = await supabase
       .from("planned_incoming_category")
       .select("value")
-      .eq("id", user_id)
+      .eq("planned_incoming_id", planned_id)
       .eq("category_id", category_id);
 
     if (error) throw error;
