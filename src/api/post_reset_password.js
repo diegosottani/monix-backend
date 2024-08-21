68e77f75f1f34840f2dafcb2be0684f3ac04e599
Diego Sottani <diegosottani@gmail.com>
2024-08-21 19:06:27 -0300

    Criação da rota de recuperar senha através de email

---
+++ src/api/post_reset_password.js
@@ -0,0 +1,19 @@
+import { supabase } from "../init";
+
+export const post_reset_password = async (req, res) => {
+  try {
+    const email = req.body.email;
+    if(!email) {
+      res.status(400).json({ error: 'É necessário informar o email' });
+    }
+
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email);
+    
+    if (error) throw error;
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+}
--- src/api/root_router.js
+++ src/api/root_router.js
@@ -4,6 +4,7 @@ import { post_login, get_google_login, post_google_callback } from './post_login
 import { post_logout } from './post_logout.js';
 import { post_refresh_token } from './post_refresh_token.js';
 import { get_users } from '../api/auth/user/get_users.js';
+import { post_reset_password } from './post_reset_password.js';
 
 export const root_router = express.Router();
 root_router.post('/signup', post_signup);
@@ -13,4 +14,5 @@ root_router.post('/refresh_token', post_refresh_token);
 root_router.get('/auth/google', get_google_login);
 root_router.post('/auth/google/callback', post_google_callback);
 root_router.get('/users', get_users);
+root_router.post('/reset_password', post_reset_password);
 
