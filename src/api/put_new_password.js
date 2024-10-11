3e61ab348c8d81245bfc0fd71a32215f488383ff
Diego <diegosottani@gmail.com>
2024-10-11 17:39:38 -0300

    Correção put_new_password para receber o token do deeplink, autenticar e redefinir senha

--- src/api/put_new_password.js
+++ src/api/put_new_password.js
@@ -2,15 +2,28 @@ import { supabase } from "../init.js";
 
 export const put_new_password = async (req, res) => {
   try {
-    const { password, authToken } = req.body;
+    const { password, access_token, refresh_token } = req.body;
 
-    if(!password && !authToken) {
+    if(!password) {
       res.status(400).json({ error: 'É necessário informar a senha' });
     }
 
-    const {error} = await supabase.auth.updateUser(authToken, { password });
-    
+    if(!access_token) {
+      res.status(403).json({ error: 'Token inválido' });
+    }
+
+    const { data, error } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
+    });
+
     if (error) throw error;
+
+    const { error: updateError } = await supabase.auth.updateUser({
+      password
+    });
+    
+    if (updateError) throw error;
 
     res.status(201).send("Senha atualizada com sucesso");
   } catch (error) {
